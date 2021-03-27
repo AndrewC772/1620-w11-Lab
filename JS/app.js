4c32a329cb111c4702dde01bea20b373f1cae577
Andrew <chen.andrew772@gmail.com>
2021-03-26 17:34:09 -0700

    Add JS for loop to verify task complete and push to completedtodos

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,10 @@
 // your JavaScript goes here, you can delete this comment
+const todolist = [{todo: "testing the script", status:"in progress"}, {todo: "JS and CSS link", status:"complete"}]
+let completedtodos = []
+for (let task of todolist) {
+    let { todo, status } = task;
+    if (status === "complete"){
+        completedtodos.push(task)
+        console.log(completedtodos)
+    }
+}
